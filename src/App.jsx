69a00d6f5b77b159377105d32f6704a458839aeb
mubarak7785 <mubarakjinabade07@gmail.com>
2023-01-09 22:54:41 +0530

    done with the page 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ function App() {
 
   return (
     <div className="App">
-      hiiii
+      <div id='main'>
+        <div id='sub1'>
+          <button> <a href="#form">Book Demo</a></button>
+        </div>
+        <div id='sub2'>
+          <img src="https://voosh.in/static/media/my_profile.61da110e7e1e80c97356.webp" alt="" />
+        </div>
+      </div>
+      <div id='form'>
+
+      </div>
     </div>
   )
 }
