b12521df4290bbd8c1fb1b77c66d93c8c6d697bc
mubarak7785 <mubarakjinabade07@gmail.com>
2023-01-10 09:09:00 +0530

    data is successfully storing to the localstorage & done with project

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,41 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formdata,setFormdata]=useState({
+    name:"",
+    email:"",
+    companay_name:"",
+    title:"",
+    password:"",
+  })
 
+  const handleChange=(e)=>{
+    const {id,value}=e.target;
+    setFormdata({...formdata,[id]:value})
+   
+  }
+
+  const submit=()=>{
+    if(formdata.name=="" || formdata.email=="" || formdata.password=="" || formdata.title=="" || formdata.companay_name==""){
+      alert("Input field can't be empty..")
+    }
+    else{
+      localStorage.setItem("Voosh_data",JSON.stringify(formdata))
+      alert("Data is added to the localstorage successfully !")
+      setFormdata({
+        name:"",
+        email:"",
+        companay_name:"",
+        title:"",
+        password:"",
+
+      }
+       
+      )
+    }
+     
+      
+  }
   return (
     <div className="App">
       <div id='main'>
@@ -19,7 +52,33 @@ function App() {
           <div id='fone'>
             <h1>Book A Demo Now</h1>
           </div>
-          <div id='ftwo'></div>
+          <div id='ftwo'>
+            <div>
+                <div id='ftwo-sub'>
+                  <p> Name*</p>
+                  <input type="text" id='name' value={formdata.name} placeholder=' Ex John Doe' onChange={handleChange} />
+                </div>
+                <div id='ftwo-sub'>
+                  <p> Email*</p>
+                  <input type="text" id='email' value={formdata.email} placeholder=' Ex mail@website.com' onChange={handleChange} />
+                </div>
+                <div id='ftwo-sub'>
+                  <p> Password*</p>
+                  <input type="password" id='password' value={formdata.password} placeholder=' Password' onChange={handleChange} />
+                </div>
+                <div id='ftwo-sub'>
+                  <p> Companay Name*</p>
+                  <input type="text" id='companay_name' value={formdata.companay_name} placeholder=' Ex XYZ PVT LTD' onChange={handleChange} />
+                </div>
+                <div id='ftwo-sub'>
+                  <p> Your title*</p>
+                  <input type="text" id='title'  value={formdata.title} placeholder=' Ex Operational Head' onChange={handleChange} />
+                </div>
+                <div id='ftwo-sub1'>
+                  <button onClick={submit}>Book Demo</button>
+                </div>
+            </div>
+          </div>
       </div>
     </div>
   )
